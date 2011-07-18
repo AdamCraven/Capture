25665442876ddfb1fa2dfff336bc7f3b9ca8f000
Adam Craven <git@adamcraven.me>
2011-07-18 14:12:30 +0100

    Test fixed.

--- test/context.js
+++ test/context.js
@@ -23,7 +23,7 @@
                     '.link': function(e) {
                         equal(e.target, $('#testElement .link')[0], 'e target is element clicked on');
                         // FIXME: Same element but not recognised as same
-                        equal(this.element, $('#testElement'), 'this.element is the viewController bound unto');
+                        equal(this.element[0], $('#testElement')[0], 'this.element is the viewController bound unto');
                     }  
                 }
             };

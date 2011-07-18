c3b9184e7750515fbb42b7ca8178cf1d231d34f4
Adam Craven <git@adamcraven.me>
2011-07-18 19:51:18 +0100

    fixme fixed.

--- test/context.js
+++ test/context.js
@@ -22,7 +22,6 @@
                 onclick : {
                     '.link': function(e) {
                         equal(e.target, $('#testElement .link')[0], 'e target is element clicked on');
-                        // FIXME: Same element but not recognised as same
                         equal(this.element[0], $('#testElement')[0], 'this.element is the viewController bound unto');
                     }  
                 }

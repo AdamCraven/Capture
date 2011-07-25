fad6ed2f8fd4e14f345adafbb1d8785f805e00c1
Adam Craven <git@adamcraven.me>
2011-07-25 19:46:22 +0200

    Semantic ordering of test file

--- test/main.js
+++ test/main.js
@@ -11,7 +11,6 @@
 	}
 
 	module("Capture", {teardown: teardown, setup: setup });
-	
 
 		test("Namespace setup", function() {
 			ok(typeof jQuery !== "undefined",'jQuery namespace initialised');
@@ -21,27 +20,10 @@
 
 		test("Basic binding to elements", function() {
 			var viewController = { init:function(){} };
-			// Attach viewController to element
 			var boundViewController = $('#testElement').capture(viewController);
 			
 			equal(boundViewController.element[0], $('#testElement')[0], 'captured element is same');
 			notEqual(viewController, boundViewController, 'The boundViewController is a new instance of viewController, and not the same');
-		});
-		
-		test("Arguments can be sent to initialise function", function() {
-			
-			var options = {};
-			var anotherValue = 'bar';
-			var viewController = { 
-				init: function(firstArg, secondArg){
-					equal(options, firstArg, 'Options object is passed to init');
-					equal(anotherValue, secondArg, 'anotherValue is pass to init');
-				} 
-			};
-			 
-			$('#testElement').capture(viewController, options, anotherValue); 
-			
-			expect(2);
 		});
 		
 	module("Initialisation", {teardown: teardown, setup: setup });
@@ -88,6 +70,22 @@
 			}
 			
 			expect(1);
+		});
+		
+		test("Extra arguments are sent to init function", function() {
+			
+			var options = {};
+			var anotherValue = 'bar';
+			var viewController = { 
+				init: function(firstArg, secondArg){
+					equal(options, firstArg, 'Options object is passed to init');
+					equal(anotherValue, secondArg, 'anotherValue is pass to init');
+				} 
+			};
+			 
+			$('#testElement').capture(viewController, options, anotherValue); 
+			
+			expect(2);
 		});
 		
 	

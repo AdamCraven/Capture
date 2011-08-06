0e1e393bae01f39546965606f5282847ed9e853b
Adam Craven <git@adamcraven.me>
2011-08-07 01:11:19 +0200

    Inheritance test.

--- test/inheritance.js
+++ test/inheritance.js
@@ -20,5 +20,31 @@
 	        notEqual(newObj, viewController, 'A new instance of view controller has been created');
 	        ok(viewController.remove, 'viewController has inherited a method from the viewControllerBase prototype');
 	    });
+	    
+	    test("Ensure when initalised, properties and objects not shared with original", function() {
+	        var picture = {
+	            init: function() {},
+	            onclick : {
+	                "#testElement" : function() {}
+	            },
+	            someProperty : {
+	                deeper : {
+	                    deepest : {}
+	                }
+	            },
+	            anArray : [1,2,3]
+	            
+	        };
+	            
+	        
+	        var newPic = $.fn.capture.view(picture);
+	        
+	        equal(newPic.init, picture.init, 'Method same');
+	        notEqual(newPic.onclick, picture.onclick, 'Object different');
+	        equal(newPic.onclick['#testElement'], picture.onclick['#testElement'], 'Method same');
+	        notEqual(newPic.anArray, picture.anArray, 'Array different');
+	        notEqual(newPic.someProperty.deeper.deepest, picture.someProperty.deeper.deepest, 'Object different');
+	        
+	    });
 	
 }());

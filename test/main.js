6ec27f6f85ab765ef9109d5d0239ef10ab2f401b
Adam Craven <git@adamcraven.me>
2011-10-09 12:31:49 +0200

    Tests to ensure exceptions are generated when called differently.

--- test/main.js
+++ test/main.js
@@ -1,85 +1,108 @@
 /*jslint*/
 
 (function(){
-	
-	
-	function teardown() {
-		$('#testElement').remove();
-	}
+    
+    
+    function teardown() {
+        $('#testElement').remove();
+    }
 
-	function setup() {
-		$('body').append('<div id="testElement"><div class="double first"></div><div class="double second"></div></div>');
-	}
+    function setup() {
+        $('body').append('<div id="testElement"><div class="double first"></div><div class="double second"></div></div>');
+    }
 
-	module("Capture", {teardown: teardown, setup: setup });
+    module("Capture", {teardown: teardown, setup: setup });
 
-		test("Namespace setup", function() {
-			ok(typeof jQuery !== "undefined",'jQuery namespace initialised');
-			ok(typeof $.fn.capture !== "undefined",'Capture plugin exists');
-		});
-	
+        test("Namespace setup", function() {
+            ok(typeof jQuery !== "undefined",'jQuery namespace initialised');
+            ok(typeof $.fn.capture !== "undefined",'Capture plugin exists');
+        });
+    
 
-		test("Basic binding to elements", function() {
-			var viewController = { init:function(){} };
-			var boundViewController = $('#testElement').capture(viewController);
-			
-			strictEqual(viewController, boundViewController, 'The boundViewController is not a new instance of viewController');
-		});
-		
-		test("Methods in the prototype are not called by capture", function() {
-			function ViewController() {
-				// Empty
-			}
-			ViewController.prototype = {
-				init : function() {
-					ok(false,'Init in prototype will never execute');
-				},
-				onclick : {
-					'#testElement' : function(e) {}
-				}
-			};
-			
-			var viewController = new ViewController();
-			var boundViewController = $('#testElement').capture(viewController);
-			
-			ok(typeof $('#testElement').data('events') === "undefined", 'valid onclick in the prototype will not bind');
-		});
-	
-	module("Initialisation", {teardown: teardown, setup: setup });
+        test("Basic binding to elements", function() {
+            var viewController = { init:function(){} };
+            var boundViewController = $('#testElement').capture(viewController);
+            
+            strictEqual(viewController, boundViewController, 'The boundViewController is not a new instance of viewController');
+        });
+        
+        test("Methods in the prototype are not called by capture", function() {
+            function ViewController() {
+                // Empty
+            }
+            ViewController.prototype = {
+                init : function() {
+                    ok(false,'Init in prototype will never execute');
+                },
+                onclick : {
+                    '#testElement' : function(e) {}
+                }
+            };
+            
+            var viewController = new ViewController();
+            var boundViewController = $('#testElement').capture(viewController);
+            
+            ok(typeof $('#testElement').data('events') === "undefined", 'valid onclick in the prototype will not bind');
+        });
+    
+    module("Initialisation", {teardown: teardown, setup: setup });
 
-		test("Extra arguments are sent to init function", function() {
-			
-			var options = {};
-			var anotherValue = 'bar';
-			var viewController = { 
-				init: function(firstArg, secondArg){
-					equal(options, firstArg, 'Options object is passed to init');
-					equal(anotherValue, secondArg, 'anotherValue is pass to init');
-				} 
-			};
-			 
-			$('#testElement').capture(viewController, options, anotherValue); 
-			
-			expect(2);
-		});
-		
-		
-		test("Object oriented style calling works", function() {
-			
-			var options = {};
-			var anotherValue = 'bar';
-			var viewController = { 
-				init: function(firstArg, secondArg){
-					equal(options, firstArg, 'Options object is passed to init');
-					equal(anotherValue, secondArg, 'anotherValue is pass to init');
-				} 
-			};
-			 
-			Capture('#testElement', viewController, options, anotherValue); 
-			
-			expect(2);
-		});
-		
-	
-	
+        test("Extra arguments are sent to init function", function() {
+            
+            var options = {};
+            var anotherValue = 'bar';
+            var viewController = { 
+                init: function(firstArg, secondArg){
+                    equal(options, firstArg, 'Options object is passed to init');
+                    equal(anotherValue, secondArg, 'anotherValue is pass to init');
+                } 
+            };
+             
+            $('#testElement').capture(viewController, options, anotherValue); 
+            
+            expect(2);
+        });
+        
+        
+        test("Object oriented style calling works", function() {
+            
+            var options = {};
+            var anotherValue = 'bar';
+            var viewController = { 
+                init: function(firstArg, secondArg){
+                    equal(options, firstArg, 'Options object is passed to init');
+                    equal(anotherValue, secondArg, 'anotherValue is pass to init');
+                } 
+            };
+             
+            Capture('#testElement', viewController, options, anotherValue); 
+            
+            expect(2);
+        });
+        
+        test("Calling 'Capture' generates exception on lack of element", function() {
+            
+            try {
+                Capture('#fakeElement', {});
+                fail('Exception not created');
+            } catch (e) {
+                ok('Exception created when when element doesn\'t exist');
+            }
+
+        });
+        
+        test("Calling '$().capture' doesnt generate exception on lack of element", function() {
+            // Because the jQuery way is to fail silently
+            try {
+                Capture('#fakeElement', {});
+                ok('No exception created when element doesn\'t exist');
+            } catch (e) {
+                fail('Exception created when element doesn\'t exist');
+            }
+
+        });
+        
+        
+    
+    
 }());

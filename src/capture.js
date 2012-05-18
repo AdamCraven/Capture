477c44632846a69d11ad12944715838344845f36
Adam Craven <git@adamcraven.me>
2012-05-19 00:11:43 +0100

    whitespace trim.

--- src/capture.js
+++ src/capture.js
@@ -1,7 +1,7 @@
 /*jshint evil: false, bitwise:false, strict: true, undef: true, white:true, onevar:false, browser:true, plusplus:false */
 /*global jQuery, Capture:true, window */
 /**
- *  Capture.js 
+ *  Capture.js
  *  v1.1.0
  *  Copyright 2012 Adam Craven
  *  Freely distributable under the MIT License.
@@ -9,20 +9,20 @@
  */
 (function ($) {
     "use strict";
-    
+
     if (typeof Capture !== "undefined") {
         throw new Error('Capture is already defined');
     }
-    
+
     if (typeof $ === "undefined") {
         throw new Error('Capture.js; jQuery is not defined.\n You need jQuery to run Capture');
     }
-    
+
     var bind = Function.prototype.bind;
     var slice = Array.prototype.slice;
     var toString = Object.prototype.toString;
     var UNDEFINED;
-    
+
     // All properties with prefix of 'on' are treated as event delegates
     var eventMethodPrefix = /^on(.+)/;
 
@@ -37,7 +37,7 @@
         }
         return $.proxy(fn, scope);
     }
-    
+
     /**
      *  Validates the view when captured, checking for basic errors
      */
@@ -49,7 +49,7 @@
         if (toString.call(view) !== '[object Object]') {
             throw new Error('Capture.js; View wrong type.\n The view must be an object or a constructor.');
         }
-        
+
         if (element.length === 0 || !element.each) {
             throw new Error('Capture.js; No element found.\n You must attach the view to an element.');
         }
@@ -58,12 +58,12 @@
     /**
      *  Adds listeners to the view.
      *  By default all methods that have the valid prefix 'on', their selectors will be bound
-     *  
+     *
      *  @example view controller method the function will bind event delegates with
-     *    onclick : { 
-     *      '.selector' : function(e) { 
-     *      
-     *      }   
+     *    onclick : {
+     *      '.selector' : function(e) {
+     *
+     *      }
      *    }
      */
     function addEventListeners(view) {
@@ -123,7 +123,7 @@
 
         return view;
     }
-    
+
     /**
      * View Controller base constructor.
      * When an object is wrapped with Capture.view, it inherits methods directly
@@ -131,7 +131,7 @@
      * @constructor
      */
     function ViewBaseClass() {}
-    
+
     ViewBaseClass.prototype = {
         // Remove all event handlers
         removeEventListeners : function () {
@@ -147,44 +147,44 @@
 
     /**
      *  Capture attaches a view to an element via event delegates.
-     *  
+     *
      *  @param {String/Object}   element Element to attach view unto.
      *  @param {Object/Function} View    To be instantiated as a capture view.
      *  @param {AnyType}         [1..*]  One or more extra arguments that are passed to the view.init.
-     *  
+     *
      *  @returns {object}    Instantiated view
-     *  
+     *
      *  @example
      *   Capture('#gallery', view)
      */
     window.Capture = function (element, view) {
         // If wrapped by Capture.view, it will be a function.
         view = (typeof view === "function") ? view() : view;
-                
+
         element = $(element);
 
         validate(element, view);
 
         return connectView(element.eq(0), view, (arguments.length > 2) ? slice.call(arguments, 2) : UNDEFINED);
     };
-    
+
     /**
      * Call Capture via more explicit attach method
      *  @example
      *   Capture.attach('#gallery', view)
      */
     Capture.attach = Capture;
-    
+
     /**
-     *  This wraps the view until called by capture. 
+     *  This wraps the view until called by capture.
      *  It instantiates the view controller and sets inheritance to view base class.
      *
      *  @param {Object/Function} View    To be instantiated as a capture view
      *
      *  @returns {function} The view to be initalised by capture
      */
-    Capture.view = function (View) { 
-        return function () {       
+    Capture.view = function (View) {
+        return function () {
             // If a constructor has been passed, instantiate it now
             var view = (typeof View === "function") ? new View() : View;
 
@@ -192,9 +192,9 @@
             return $.extend(true, new ViewBaseClass(), view);
         };
     };
-    
+
     /**
-     *  jQuery method to capture a view on the existing element. 
+     *  jQuery method to capture a view on the existing element.
      *  Doesn't return an error if element doesn't exist, as per jQuery philosophy.
      *
      *  @param  {object}  view  The View to be initialised from on the element
@@ -211,5 +211,5 @@
         }
         return window.Capture.apply(null, [this].concat(slice.call(arguments, 0)));
     };
-    
+
 })(jQuery);
